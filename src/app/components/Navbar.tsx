f8d355d5d0f8b95cc9f7e6e8731a699b19ee0195
Akash Jindal <20akash.jindal@gmail.com>
2025-04-13 19:07:33 +0530

    added home button navigation

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -32,18 +32,19 @@ const Navbar = () => {
   return (
     <div
       className={`flex items-center justify-between p-4 sticky top-0 bg-[var(--foreground)] z-10 transition-shadow duration-300 ${!scrolled
-          ? "shadow-[0_10px_50px_-15px_rgba(86,227,159,0.1)]"
-          : "shadow-[0_20px_30px_-15px_rgba(0,0,0,0.3)]"
+        ? "shadow-[0_10px_50px_-15px_rgba(86,227,159,0.1)]"
+        : "shadow-[0_20px_30px_-15px_rgba(0,0,0,0.3)]"
         }`}
     >
-      <div>
+      <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className="cursor-pointer">
         <House color="white" />
       </div>
+
       <div className="flex gap-5 items-center">
         <Link href={"https://www.linkedin.com/in/akash-jindal-2258771a9/"} target="_blank"><Linkedin color="white" /></Link>
         <Link href={"https://github.com/Mate-20/MyProjects"} target="_blank"><Github color="white" /></Link>
         <motion.button
-        whileTap={{scale : 0.9}}
+          whileTap={{ scale: 0.9 }}
           onClick={handleDownload}
           className="border cursor-pointer border-[var(--prim-text)] rounded text-[var(--prim-text)] px-4 py-1 
              transition-colors duration-300 ease-in-out hover:bg-[#56E39F] hover:text-black"

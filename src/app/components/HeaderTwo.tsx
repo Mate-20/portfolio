993d49b59441bb6d7e591b5bd9a327ccc209c821
Akash Jindal <20akash.jindal@gmail.com>
2025-04-19 13:18:13 +0530

    intro updated

--- src/app/components/HeaderTwo.tsx
+++ src/app/components/HeaderTwo.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 const HeaderTwo = () => {
   const lines = [
     "Hi",
-    "My Name is Akash Jindal. I am a Software Developer with 2 Years of Experience.",
+    "My Name is Akash Jindal. I am a Software Developer with 2 Years of Industry Experience.",
     "Skilled in React, Next.js, Framer Motion, and much more.",
     "Always learning, always building 🚀"
   ]
